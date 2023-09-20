0cd2bd3d84020a0df5c0d1d67c8ad8e0ee00f760
Bas Westerbaan <bas@westerbaan.name>
2023-09-20 15:35:11 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bwesterb/pq-tester
+
+go 1.21.1
---
+++ main.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+)
+
+type ConnContextKey struct{}
+
+type Conn struct {
+	kex tls.CurveID
+	hrr bool
+}
+
+func (c *Conn) eventHandler(ev tls.CFEvent) {
+	switch e := ev.(type) {
+	case tls.CFEventTLS13HRR:
+		c.hrr = true
+	case tls.CFEventTLS13NegotiatedKEX:
+		c.kex = e.KEX
+	}
+}
+
+func errResp(w http.ResponseWriter, status int, msg string, args ...any) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(status)
+	fmt.Fprintf(w, msg, args...)
+}
+
+func handler(w http.ResponseWriter, req *http.Request) {
+	ctx := req.Context()
+	if req.Method == "POST" {
+		err := req.ParseForm()
+		if err != nil {
+			errResp(w, 400, "can't parse form: %v", err)
+			return
+		}
+		newConn := &Conn{}
+		remote := req.PostFormValue("remote")
+		newCtx := context.WithValue(
+			ctx,
+			tls.CFEventHandlerContextKey{},
+			newConn.eventHandler,
+		)
+		remoteHost, _, err := net.SplitHostPort(remote)
+		if err != nil {
+			errResp(w, 400, "can't parse remote: %v", err)
+			return
+		}
+		tcpConn, err := (&net.Dialer{}).DialContext(
+			newCtx,
+			"tcp",
+			remote,
+		)
+		if err != nil {
+			errResp(w, 400, "can't dial: %v", err)
+			return
+		}
+		defer tcpConn.Close()
+		method := req.PostFormValue("method")
+
+		curves := []tls.CurveID{
+			tls.X25519,
+			tls.CurveP256,
+			tls.CurveP384,
+			tls.X25519Kyber768Draft00,
+		}
+
+		if method == "" || method == "supported" {
+		} else if method == "preferred" {
+			curves = []tls.CurveID{
+				tls.X25519Kyber768Draft00,
+				tls.X25519,
+				tls.CurveP256,
+				tls.CurveP384,
+			}
+		} else {
+			errResp(w, 400, "unknown method")
+			return
+		}
+		serverName := remoteHost
+		if req.PostFormValue("servername") != "" {
+			serverName = req.PostFormValue("servername")
+		}
+
+		insecure := req.PostFormValue("insecure") != ""
+
+		conn := tls.Client(tcpConn, &tls.Config{
+			CurvePreferences:   curves,
+			ServerName:         serverName,
+			InsecureSkipVerify: insecure,
+		})
+
+		defer conn.Close()
+		err = conn.HandshakeContext(newCtx)
+		if err != nil {
+			errResp(w, 400, "handshake: %v", err)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		ret := struct {
+			Kex    tls.CurveID
+			HRR    bool
+			Remote string
+		}{
+			Kex:    newConn.kex,
+			HRR:    newConn.hrr,
+			Remote: remote,
+		}
+		json.NewEncoder(w).Encode(&ret)
+
+		return
+	}
+	conn := ctx.Value(ConnContextKey{}).(*Conn)
+	w.Header().Set("Content-Type", "application/json")
+	ret := struct {
+		Kex tls.CurveID
+		HRR bool
+	}{
+		Kex: conn.kex,
+		HRR: conn.hrr,
+	}
+	json.NewEncoder(w).Encode(&ret)
+}
+
+func main() {
+	addr := flag.String("addr", "0.0.0.0:4433", "Address to bind to")
+	cert := flag.String("cert", "server.crt", "Path to certificate")
+	key := flag.String("key", "server.key", "Path to key")
+
+	http.HandleFunc("/", handler)
+
+	log.Printf("Listening on %s", *addr)
+	srv := http.Server{
+		Addr: *addr,
+		TLSConfig: &tls.Config{
+			CurvePreferences: []tls.CurveID{
+				tls.X25519Kyber768Draft00,
+				tls.X25519,
+				tls.CurveP256,
+				tls.CurveP384,
+			},
+		},
+		ConnContext: func(ctx context.Context, _ net.Conn) context.Context {
+			conn := &Conn{}
+			return context.WithValue(
+				context.WithValue(
+					ctx,
+					tls.CFEventHandlerContextKey{},
+					conn.eventHandler,
+				),
+				ConnContextKey{},
+				conn,
+			)
+		},
+	}
+
+	if err := srv.ListenAndServeTLS(*cert, *key); err != nil {
+		log.Fatalf("ListenAndServeTLS: %v", err)
+	}
+}
