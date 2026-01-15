1b2d1e27bbeba2dfc09621e54faf504d6c25a165
Bas Westerbaan <bas@cloudflare.com>
2026-01-15 14:57:13 +0100

    Also test client, which requires TLS

--- main.go
+++ main.go
@@ -28,6 +28,67 @@ func isPQ(kex tls.CurveID) bool {
 	return false
 }
 
+func clientTest(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	if req.TLS == nil {
+		errResp(w, 400, "not connecting over TLS")
+		return
+	}
+	ret := struct {
+		Kex tls.CurveID
+	}{
+		Kex: req.TLS.CurveID,
+	}
+	json.NewEncoder(w).Encode(&ret)
+}
+
+func remoteTest(w http.ResponseWriter, req *http.Request) {
+	remote := req.PostFormValue("remote")
+	remoteHost, _, err := net.SplitHostPort(remote)
+	if err != nil {
+		errResp(w, 400, "can't parse remote: %v", err)
+		return
+	}
+	tcpConn, err := net.Dial("tcp", remote)
+	if err != nil {
+		errResp(w, 400, "can't dial: %v", err)
+		return
+	}
+	defer tcpConn.Close()
+
+	serverName := remoteHost
+	if req.PostFormValue("servername") != "" {
+		serverName = req.PostFormValue("servername")
+	}
+
+	insecure := req.PostFormValue("insecure") != ""
+
+	conn := tls.Client(tcpConn, &tls.Config{
+		ServerName:         serverName,
+		InsecureSkipVerify: insecure,
+	})
+
+	defer conn.Close()
+	err = conn.Handshake()
+	if err != nil {
+		errResp(w, 400, "handshake: %v", err)
+		return
+	}
+
+	state := conn.ConnectionState()
+	w.Header().Set("Content-Type", "application/json")
+	ret := struct {
+		Kex    tls.CurveID
+		Remote string
+		PQ     bool
+	}{
+		Kex:    state.CurveID,
+		Remote: remote,
+		PQ:     isPQ(state.CurveID),
+	}
+	json.NewEncoder(w).Encode(&ret)
+}
+
 func handler(w http.ResponseWriter, req *http.Request) {
 	if req.Method == "POST" {
 		err := req.ParseForm()
@@ -35,51 +96,12 @@ func handler(w http.ResponseWriter, req *http.Request) {
 			errResp(w, 400, "can't parse form: %v", err)
 			return
 		}
-		remote := req.PostFormValue("remote")
-		remoteHost, _, err := net.SplitHostPort(remote)
-		if err != nil {
-			errResp(w, 400, "can't parse remote: %v", err)
-			return
-		}
-		tcpConn, err := net.Dial("tcp", remote)
-		if err != nil {
-			errResp(w, 400, "can't dial: %v", err)
-			return
-		}
-		defer tcpConn.Close()
-
-		serverName := remoteHost
-		if req.PostFormValue("servername") != "" {
-			serverName = req.PostFormValue("servername")
-		}
-
-		insecure := req.PostFormValue("insecure") != ""
-
-		conn := tls.Client(tcpConn, &tls.Config{
-			ServerName:         serverName,
-			InsecureSkipVerify: insecure,
-		})
-
-		defer conn.Close()
-		err = conn.Handshake()
-		if err != nil {
-			errResp(w, 400, "handshake: %v", err)
+		if req.PostFormValue("remote") != "" {
+			remoteTest(w, req)
 			return
 		}
 
-		state := conn.ConnectionState()
-		w.Header().Set("Content-Type", "application/json")
-		ret := struct {
-			Kex    tls.CurveID
-			Remote string
-			PQ     bool
-		}{
-			Kex:    state.CurveID,
-			Remote: remote,
-			PQ:     isPQ(state.CurveID),
-		}
-		json.NewEncoder(w).Encode(&ret)
-
+		clientTest(w, req)
 		return
 	}
 
@@ -90,17 +112,30 @@ func handler(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 	addr := flag.String("addr", "0.0.0.0:8080", "Address to bind to")
+	certFile := flag.String("cert", "", "Path to TLS certificate file")
+	keyFile := flag.String("key", "", "Path to TLS private key file")
 
 	flag.Parse()
 
 	http.HandleFunc("/", handler)
 
-	log.Printf("Listening on %s", *addr)
 	srv := http.Server{
 		Addr: *addr,
 	}
 
-	if err := srv.ListenAndServe(); err != nil {
-		log.Fatalf("ListenAndServe: %v", err)
+	if *certFile != "" && *keyFile != "" {
+		log.Printf("Listening on %s (TLS)", *addr)
+		if err := srv.ListenAndServeTLS(*certFile, *keyFile); err != nil {
+			log.Fatalf("ListenAndServeTLS: %v", err)
+		}
 	}
+
+	if *certFile == "" && *keyFile == "" {
+		log.Printf("Listening on %s", *addr)
+		if err := srv.ListenAndServe(); err != nil {
+			log.Fatalf("ListenAndServe: %v", err)
+		}
+	}
+
+	log.Fatalf("-cert and -key must be set together")
 }
